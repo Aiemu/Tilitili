ff4fb50d98ced397f7829264496b6a0fc083755f
Aiemu <479983336@qq.com>
2020-06-18 16:27:14 +0800

    update config

--- tilitili/app/src/main/java/com/example/tilitili/Config.java
+++ tilitili/app/src/main/java/com/example/tilitili/Config.java
@@ -1,6 +1,6 @@
 package com.example.tilitili;
 
 public class Config {
-    public static final String host = "http://192.168.0.104";
+    public static final String host = "http://127.0.0.1";
     public static final String port = "8000";
 }
--- tilitili/app/src/main/java/com/example/tilitili/LoginActivity.java
+++ tilitili/app/src/main/java/com/example/tilitili/LoginActivity.java
@@ -53,10 +53,10 @@ public class LoginActivity extends Activity {
         Map<String, String> map = new HashMap<>(2);
         map.put("username", username);
         map.put("password", password);
-        
+
         // for test
-        Intent register_intent = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(register_intent);
+//        Intent register_intent = new Intent(LoginActivity.this, MainActivity.class);
+//        startActivity(register_intent);
 
         SpotsCallBack<String> stringSpotsCallBack = new SpotsCallBack<String>(this) {
             @Override
