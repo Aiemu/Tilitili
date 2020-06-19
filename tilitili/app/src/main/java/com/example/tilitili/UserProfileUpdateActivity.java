d2739c78e878da2406f1df25fc007a759b32c61a
Aiemu <479983336@qq.com>
2020-06-19 21:15:04 +0800

    delete spinner

--- tilitili/app/src/main/java/com/example/tilitili/UserProfileUpdateActivity.java
+++ tilitili/app/src/main/java/com/example/tilitili/UserProfileUpdateActivity.java
@@ -11,8 +11,6 @@ import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
 
 public class UserProfileUpdateActivity extends Activity {
-    @ViewInject(R.id.department_spinner)
-    private Spinner departmentSpinner;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,6 +20,5 @@ public class UserProfileUpdateActivity extends Activity {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.departments, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        departmentSpinner.setAdapter(adapter);
     }
 }
